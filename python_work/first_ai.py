b2b20b77e41cf9041f5215d469ab17d115abe1ae
xiong35 <2242787668@qq,com>
2019-11-30 18:01:34 +0800

    ai 2.0

--- python_work/first_ai.py
+++ python_work/first_ai.py
@@ -1,3 +1,25 @@
+import os
+import struct
+import numpy as np
+
+def load_mnist(path, kind='train'):
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte'
+                               % kind)
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II',lbpath.read(8))
+        labels = np.fromfile(lbpath,dtype=np.uint8)
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII',
+                                               imgpath.read(16))
+        images = np.fromfile(imgpath,
+                             dtype=np.uint8).reshape(len(labels), 784)
+    return images, labels
+
 import numpy as np
 np.random.seed(7)  
 from keras.datasets import mnist
@@ -6,13 +28,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import RMSprop
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0], -1) / 255. 
-X_test = X_test.reshape(X_test.shape[0], -1) / 255.  
+x_train = x_train.reshape(x_train.shape[0], -1) / 255. 
+x_test = x_test.reshape(x_test.shape[0], -1) / 255.  
 y_train = np_utils.to_categorical(y_train, num_classes=10)
 y_test = np_utils.to_categorical(y_test, num_classes=10)
-
 
 model = Sequential() 
 
@@ -23,9 +44,9 @@ model.add(Dense(10,activation='softmax'))
 model.compile(optimizer='adam',loss=
 'categorical_crossentropy',metrics=['accuracy'])
 
-model.fit(x_train,y_batch,batch_size=100,epochs=20)
+model.fit(x_train,y_train,batch_size=100,epochs=20)
 
 result = model.evaluate(x_train,y_train,batch_size = 10000)
 
-print('test loss: ', recule[0])
+print('test loss: ', result[0])
 print('test accuracy: ', result[1])
