e62e99454c4eb7b7317055753483d8e47010c27a
xiong35 <2242787668@qq.com>
2020-01-15 18:17:14 +0800

    imdb comment ai

---
+++ python_work/imdb.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from keras import layers
+from keras import models
+import numpy as np
+from keras.datasets import imdb
+(train_data, train_labels), (test_data,
+                             test_labels) = imdb.load_data(num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+
+model.add(layers.Dense(32, activation='relu', input_shape=(10000,)))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(8, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='mse',
+              metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train, partial_y_train, epochs=12,
+                    batch_size=512, validation_data=(x_val, y_val))
+
+print(model.predict(x_test))
+
+################################################################
+
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(loss_values)+1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Acc')
+plt.legend()
+plt.show()
+
+""" word_index=imdb.get_word_index()
+reverse_word_index=dict([(value,key) for (key,value) in word_index.items()])
+decoded_review=" ".join([reverse_word_index.get(i-3,"?") for i in train_data[0]])  
+print(decoded_review) """
