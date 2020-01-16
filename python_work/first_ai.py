f41e710c3a4b6734b9bbf7d353c0ecb73fcd3797
xiong35 <2242787668@qq.com>
2020-01-16 19:01:11 +0800

    fmt

--- python_work/first_ai.py
+++ python_work/first_ai.py
@@ -1,6 +1,12 @@
+from keras.optimizers import RMSprop
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.datasets import mnist
 import os
 import struct
 import numpy as np
+
 
 def load_mnist(path, kind='train'):
     """Load MNIST data from `path`"""
@@ -11,8 +17,8 @@ def load_mnist(path, kind='train'):
                                '%s-images-idx3-ubyte'
                                % kind)
     with open(labels_path, 'rb') as lbpath:
-        magic, n = struct.unpack('>II',lbpath.read(8))
-        labels = np.fromfile(lbpath,dtype=np.uint8)
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
     with open(images_path, 'rb') as imgpath:
         magic, num, rows, cols = struct.unpack('>IIII',
                                                imgpath.read(16))
@@ -20,33 +26,28 @@ def load_mnist(path, kind='train'):
                              dtype=np.uint8).reshape(len(labels), 784)
     return images, labels
 
-import numpy as np
-np.random.seed(7)  
-from keras.datasets import mnist
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.optimizers import RMSprop
+
+np.random.seed(7)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(x_train.shape[0], -1) / 255. 
-x_test = x_test.reshape(x_test.shape[0], -1) / 255.  
+x_train = x_train.reshape(x_train.shape[0], -1) / 255.
+x_test = x_test.reshape(x_test.shape[0], -1) / 255.
 y_train = np_utils.to_categorical(y_train, num_classes=10)
 y_test = np_utils.to_categorical(y_test, num_classes=10)
 
-model = Sequential() 
+model = Sequential()
 
-model.add(Dense(666,activation='relu',input_shape=(784,)))
-model.add(Dense(666,activation='relu'))
-model.add(Dense(10,activation='softmax'))
+model.add(Dense(666, activation='relu', input_shape=(784,)))
+model.add(Dense(666, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer='adam',loss=
-'categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-model.fit(x_train,y_train,batch_size=100,epochs=20)
+model.fit(x_train, y_train, batch_size=100, epochs=20)
 
-result = model.evaluate(x_train,y_train,batch_size = 10000)
+result = model.evaluate(x_train, y_train, batch_size=10000)
 
 print('test loss: ', result[0])
 print('test accuracy: ', result[1])
