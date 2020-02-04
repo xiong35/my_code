4cdb66a8a0027e77786d06155df8d24234d04254
xiong35 <2242787668@qq.com>
2020-02-04 12:33:43 +0800

    tree done

--- python_work/ML_in_action/decision_tree.py
+++ python_work/ML_in_action/decision_tree.py
@@ -2,6 +2,7 @@
 
 # calculate the entropy of the data
 
+import operator
 from math import log
 
 
@@ -51,16 +52,21 @@ def splitDataSet(dataSet, axis, value):
 # and find the best way to split the data
 
 def chooseBestFeatureToSplit(dataSet):
+    # in this case, the dataSet's last value is the y value
+    # no need to be taken into consideration
     numFeatures = len(dataSet[0])-1
+    # set original value
     bestEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0
     bestFeature = -1
     for i in range(numFeatures):
+        # set the feature in i axis
         featList = [example[i] for example in dataSet]
         uniqueVals = set(featList)
         newEntropy = 0.0
         for value in uniqueVals:
-            subDataSet = splitDataSet(dataSet,i,value)
+            subDataSet = splitDataSet(dataSet, i, value)
+            # calculate posterior prob
             prob = len(subDataSet)/float(len(dataSet))
             newEntropy += prob*calcShannonEnt(subDataSet)
         infoGain = bestEntropy - newEntropy
@@ -68,3 +74,48 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+
+# return the name of the most frequent class
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# recursively split the data,
+# according to the decreasement of the entropy
+# return when all features are split
+# or the subDataSet are all the same
+
+def createTree(dataSet, labels):
+    # in this case, the dataSet's last value is the y value
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet
+                                                 (dataSet, bestFeat, value), subLabels)
+    return myTree
+
+myDat,labels = createDataSet()
+
+myTree = createTree(myDat,labels)
+
+print(myTree)
