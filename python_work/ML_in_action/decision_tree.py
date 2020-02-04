3dd4ab9ed5ab80a632fb6b7ecf6e7f6611aae08e
xiong35 <2242787668@qq.com>
2020-02-04 17:56:13 +0800

    decision tree

--- python_work/ML_in_action/decision_tree.py
+++ python_work/ML_in_action/decision_tree.py
@@ -2,6 +2,7 @@
 
 # calculate the entropy of the data
 
+import matplotlib.pyplot as plt
 import operator
 from math import log
 
@@ -24,12 +25,16 @@ def calcShannonEnt(dataSet):
 # creat dataSet
 
 def createDataSet():
-    dataSet = [[1, 1, 'yes'],
-               [1, 1, 'yes'],
-               [1, 0, 'no'],
-               [0, 1, 'no'],
-               [0, 1, 'no']]
-    labels = ['no surfacing', 'flippers']
+    dataSet = [[1, 1, 0,'yes'],
+               [1, 1, 0,'yes'],
+               [1, 0, 0,'no'],
+               [0, 1, 0,'no'],
+               [0, 1, 1,'no'],
+               [0, 1, 1,'yes'],
+               [1, 0, 0,'no'],
+               [1, 0, 1,'yes'],
+               [0, 0, 0,'no']]
+    labels = ['no surfacing', 'flippers','true']
     return dataSet, labels
 
 
@@ -111,11 +116,131 @@ def createTree(dataSet, labels):
     for value in uniqueVals:
         subLabels = labels[:]
         myTree[bestFeatLabel][value] = createTree(splitDataSet
-                                                 (dataSet, bestFeat, value), subLabels)
+                                                  (dataSet, bestFeat, value), subLabels)
     return myTree
 
-myDat,labels = createDataSet()
 
-myTree = createTree(myDat,labels)
+# createTree
 
+myDat, labels = createDataSet()
+myTree = createTree(myDat, labels)
 print(myTree)
+
+
+###############################
+#####--- show the tree ---#####
+###############################
+
+
+decisionNode = dict(boxstyle='sawtooth', fc='0.8')
+leafNode = dict(boxstyle='round4', fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    plt.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+
+def plotMidText(centerPt, parentPt, txtString):
+    xMid = (parentPt[0] - centerPt[0]) / 2.0 + centerPt[0]
+    yMid = (parentPt[1] - centerPt[1]) / 2.0 + centerPt[1]
+    plt.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    centerPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW,
+                plotTree.yOff)
+    plotMidText(centerPt, parentPt, nodeTxt)
+    plotNode(firstStr, centerPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], centerPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff,
+                                       plotTree.yOff), centerPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), centerPt,str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    plt.axis('off')
+    plt.subplots(111, frameon=False)
+    plt.subplots()
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
+
+createPlot(myTree) 
+
+
+########################
+#####---classify---#####
+########################
+
+def calssify(inputTree,featLabels,testVec):
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ =='dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+##########################
+#####---save model---#####
+##########################
+
+def storeTree(inputTree,filename):
+    import pickle
+    fw = open(filename,'wb')
+    pickle.dump(inputTree,fw)
+    fw.close()
+
+def loadTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
+
+storeTree(myTree,'tree.bin')
+print(loadTree('tree.bin'))
