7925b7edb171777ef8f1782777bb65eb316831d2
xiong35 <2242787668@qq.com>
2020-02-09 21:10:04 +0800

    VGG model

---
+++ python_work/VGG16.py
@@ -0,0 +1,59 @@
+from keras import models, layers, optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import os
+from keras.applications import VGG16
+
+
+conv_base = VGG16(weights='imagenet', include_top=False,
+                  input_shape=(150, 150, 3))
+
+base_dir = '/home/ylxiong/Documents/base_dir'
+train_dir = os.path.join(base_dir, 'train')
+val_dir = os.path.join(base_dir, 'val')
+test_dir = os.path.join(base_dir, 'test')
+
+dataGen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = dataGen.flow_from_directory(
+        directory,
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+    i = 0
+    for input_batch, labels_batch in generator:
+        features_batch = conv_base.predict(input_batch)
+        features[i*batch_size:(i+1)*batch_size] = features_batch
+        labels[i*batch_size:(i+1)*batch_size] = labels_batch
+        i += 1
+        if i*batch_size >= sample_count:
+            break
+    return features, labels
+
+
+train_features, train_labels = extract_features(train_dir, 2000)
+val_features, val_labels = extract_features(val_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4*4*512))
+val_features = np.reshape(val_features, (1000, 4*4*512))
+test_features = np.reshape(test_features, (1000, 4*4*512))
+
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4*4*512))
+model.add(layers.Dropout(0.6))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy', metrics=['acc'])
+
+
+history = model.fit(train_features, train_labels,
+                    epochs=30, batch_size=20,
+                    validation_data=(val_features, val_labels))
