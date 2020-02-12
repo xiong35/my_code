3b3bd5c8f7fe46c960f159dd3863e212b1bab35d
xiong35 <2242787668@qq.com>
2020-02-12 20:06:34 +0800

    GRU plt

--- ML_works/weather_LSTM/weather_LSTM.py
+++ ML_works/weather_LSTM/weather_LSTM.py
@@ -107,3 +107,26 @@ test_gen = (len(float_data)-300001 - lookback)//batch_size
 
 ##### train a GRU with dropout #####
 
+model = Sequential()
+model.add(layers.GRU(32, dropout=0.2, recurrent_dropout=0.2
+                     input_shape=(None, float_data.shape[-1])))
+
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mse')
+history = model.fit_generator(train_gen, steps_per_epoch=500,
+                              epochs=40, validation_data=val_gen,
+                              validation_steps=val_steps)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss)+1)
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.savefig('/images/GRU_mpl')
