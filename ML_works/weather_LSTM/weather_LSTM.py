5870537cf53405311e3f09a5ee3967a58f9e4cfb
xiong35 <2242787668@qq.com>
2020-02-13 15:53:07 +0800

    2.0

--- ML_works/weather_LSTM/weather_LSTM.py
+++ ML_works/weather_LSTM/weather_LSTM.py
@@ -122,16 +122,19 @@ test_steps = (len(float_data) - 300001 - lookback) // batch_size
 
 model = Sequential()
 model.add(layers.GRU(32,
-                     dropout=0.3,
+                     dropout=0.2,
                      # recurrent_dropout=0.2,
+                     return_sequences=True,
                      input_shape=(None, float_data.shape[-1])))
+model.add(layers.GRU(64, activation='relu', dropout=0.2))
+model.add(layers.Dense(64))
 model.add(layers.Dense(1))
 model.compile(optimizer=RMSprop(), loss='mae')
 history = model.fit_generator(train_gen,
-                              steps_per_epoch=500,
-                              epochs=40,
+                              steps_per_epoch=5,#TD
+                              epochs=5,#TD
                               validation_data=val_gen,
-                              validation_steps=val_steps)
+                              validation_steps=4)#TD
 
 
 loss = history.history['loss']
@@ -142,4 +145,4 @@ plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
-plt.savefig('./images/GRU_mpl')
+plt.savefig('./images/GRU_mpl2.0')
