1881f14621a4b235744a60b81de34aff0cbd60b2
xiong35 <2242787668@qq.com>
2020-02-14 11:43:27 +0800

    forcast

--- ML_works/weather_LSTM/forcast.py
+++ ML_works/weather_LSTM/forcast.py
@@ -1,13 +1,100 @@
-from weather_LSTM import test_gen
+
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import load_model
 
+
+data_dir = '/root/MySource/jena'
+
+fname = os.path.join(data_dir, 'climate.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+float_data = np.zeros((len(lines), len(header) - 1))
+
+
+# the first column of the data is Date
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values
+
+
+# standardize the data
+# take the first 200000 time steps as training data
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
+print('data loaded')
+
 my_model = load_model('/root/MySource/GRU.h5')
+print('model loaded')
+
+
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+            num of[output for the next procces]
+        samples = np.zeros((len(rows),
+                            lookback // step,
+                            data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        # samples = np.zeros((len(rows),          # num of [input for the next procces]
+        yield samples, targets
+        #                     lookback // step,   # num of previous data
+        #                     data.shape[-1]))    # num of features
+        # num of [output for the next procces]
+        samples = np.zeros((len(rows),
+                            lookback // step,
+                            data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
 
 x_test, y_test = next(test_gen)
+print(len(y_test))
 
 predictions = my_model.predict(x_test)
 
 print(len(predictions))
-print(len(y_test))
