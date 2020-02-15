b23cdc70a7443b329461e2b96caa90ab2485975e
xiong35 <2242787668@qq.com>
2020-02-15 09:27:31 +0800

    NaN

--- ML_works/weather_LSTM/weather_LSTM.py
+++ ML_works/weather_LSTM/weather_LSTM.py
@@ -103,13 +103,13 @@ test_steps = (len(float_data) - 300001 - lookback) // batch_size
 
 model = Sequential()
 model.add(layers.LSTM(32,
-                      dropout=0.1,
-                      recurrent_dropout=0.5,
+                      dropout=0.25,
+                      # recurrent_dropout=0.5,
                       return_sequences=True,
                       input_shape=(None, float_data.shape[-1],
                                    )))
 model.add(layers.LSTM(64, activation='relu', 
-                      dropout=0.1,recurrent_dropout=0.5))
+                      dropout=0.25))
 model.add(layers.Dense(1))
 
 model.compile(optimizer=Adam(), loss='mae')
@@ -122,7 +122,7 @@ history = model.fit_generator(train_gen,
 
 model.save('/root/MySource/LSTM.h5')
 
-loss = history.history['loss']
+lss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(loss) + 1)
 plt.figure()
