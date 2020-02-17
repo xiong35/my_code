878fe960998b140a86df8ff160574d8412b4ccbe
xiong35 <2242787668@qq.com>
2020-02-17 16:22:25 +0800

    temp == 0.5

--- ML_works/generative_model/gen_LSTM.py
+++ ML_works/generative_model/gen_LSTM.py
@@ -66,26 +66,31 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 
+random.seed(7)
+
 for epoch in range(1, 60):
     print('epoch: ', epoch)
-    model.fit(x, y, batch_size=12, epochs=1)
+    model.fit(x, y, batch_size=128, epochs=1)
     start_index = random.randint(0, len(text)-maxlen-1)
     generated_text = text[start_index:start_index+maxlen]
     print('---generate with: "', generated_text, '"')
+    """ 
     for temperature in [0.2, 0.5, 1.0, 1.2]:
         print('------temperature: ', temperature)
         sys.stdout.write(generated_text)
+     """
+    temperature = 0.5    
 
-        for i in range(400):
-            sampled = np.zeros((1, maxlen, len(unique_chars)))
-            for t, char in enumerate(generated_text):
-                sampled[0, t, char_indices[char]] = 1
+    for i in range(400):
+        sampled = np.zeros((1, maxlen, len(unique_chars)))
+        for t, char in enumerate(generated_text):
+            sampled[0, t, char_indices[char]] = 1
 
-            preds = model.predict(sampled, verbose=1)[0]
-            next_index = sample(preds, temperature)
-            next_char = unique_chars[next_index]
+        preds = model.predict(sampled, verbose=0)[0]
+        next_index = sample(preds, temperature)
+        next_char = unique_chars[next_index]
 
-            generated_text += next_char
-            generated_text = generated_text[1:]
+        generated_text += next_char
+        generated_text = generated_text[1:]
 
-            sys.stdout.write(next_char)
+        sys.stdout.write(next_char)
