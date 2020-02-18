8ee863b874277b241f1e816dd9ac146018b5034f
xiong35 <2242787668@qq.com>
2020-02-18 09:43:03 +0800

    deep dream

--- ML_works/deep_dream/deep_dream.py
+++ ML_works/deep_dream/deep_dream.py
@@ -1,11 +1,15 @@
 import numpy as np
-from keras.applications import inception_v3
 from keras import backend as K
+from keras.applications import inception_v3
+
+model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
+
+
+model.summary()
+input()
 
 # disable training
 K.set_learning_phase(0)
-
-model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
 
 layer_contributions = {
     'mixed2': 0.2,
