6d248ebb64bf2ba6aa089dda1f26f4413b493d66
xiong35 <2242787668@qq.com>
2020-03-02 00:00:38 +0800

    0 normalize

--- ML_works/lian_chuang/0_knn.py
+++ ML_works/lian_chuang/0_knn.py
@@ -4,51 +4,90 @@ import numpy as np
 
 class knn:
     filename = None
+    testFeat = None
+    testLabels = None
 
     def __init__(self, filename):
         self.filename = filename
         pass
 
+    def line2Data(self, line):
+        line = line.strip('\n')
+        line = line.split(',')
+        curLine = []
+        for feat in line[:-1]:
+            curLine.append(float(feat))
+        if str(line[-1]) == 'Iris-setosa':
+            curLine.append(-1)
+        elif str(line[-1]) == 'Iris-versicolor':
+            curLine.append(0)
+        elif str(line[-1]) == 'Iris-virginica':
+            curLine.append(1)
+        return curLine
     # 处理数据 done
+
     def file2matrix(self):
         with open(self.filename) as fr:
-            arrayOLines = fr.readlines()
-            # numOflines = len(arrayOLines)
-            # numOfFeat = len(arrayOLines[0])-1
+            arrayOLines = fr.readlines()[:-1]
+            numOflines = len(arrayOLines)
+            numOfTestData = int(numOflines*0.2)
+            np.random.seed(7)
+            # 随机抽测试数据
+            indexOfTest = np.random.randint(0, numOflines, numOfTestData)
+            testFeat = []
+            testLabels = []
+            # 将测试数据整合成矩阵
+            for index in indexOfTest:
+                line = self.line2Data(arrayOLines[index])
+                testFeat.append(line[:-1])
+                testLabels.append(line[-1])
+            self.testFeat = np.matrix(testFeat)
+            self.testLabels = np.matrix(testLabels)
+            # 将"训练"数据整合成矩阵
             retMat = []
             retLabels = []
-            for line in arrayOLines:
-                line = line.strip('\n')
-                print(line)
-                if not line:
+            for index in range(numOflines):
+                if index in indexOfTest:
                     continue
-                line = line.split(',')
-                curLine = []
-                for feat in line[:-1]:
-                    curLine.append(float(feat))
-                retMat.append(curLine)
-                if str(line[-1]) == 'Iris-setosa':
-                    retLabels.append(-1)
-                elif str(line[-1]) == 'Iris-versicolor':
-                    retLabels.append(0)
-                else:
-                    retLabels.append(1)
-            # retLabels = np.mat(retLabels)
+                line = self.line2Data(arrayOLines[index])
+                retMat.append(line[:-1])
+                retLabels.append(line[-1])
             retMat = np.matrix(retMat)
+            retLabels = np.matrix(retLabels)
             return retMat, retLabels
 
     def normalize(self):
         mat, labels = self.file2matrix()
-        print(mat)
         mean = mat.mean(axis=0)
         mat -= mean
         std = mat.std(axis=0)
         mat /= std
+
+        self.testFeat -= mean
+        self.testFeat /= std
+        print(self.testFeat)
         return mat, labels
 
     # 计算测试样本与所有训练样本的距离
-    def calculateDist(self):
-        pass
+    # def calculateDist(self):
+    #     # def classify(inX, dataSet, labels, k):
+    #     data, labels = self.normalize()
+    #     dataSize = data.shape[0]
+    #     loss = 0
+
+    #     dataSize = dataSet.shape[0]
+    #     diffMat = np.tile(inX, (dataSize, 1)) - dataSet
+    #     sqdiffMat = diffMat ** 2
+    #     sqDistance = sqdiffMat.sum(axis=1)
+    #     distances = sqDistance ** 0.5
+    #     sortedDist = distances.argsort()
+    #     classCount = {}
+    #     for i in range(k):
+    #         voteIlable = labels[sortedDist[i]]
+    #         classCount[voteIlable] = classCount.get(voteIlable, 0) + 1
+    #     sortedClassCount = sorted(classCount.items(),
+    #                               key=operator.itemgetter(1), reverse=True)
+    #     return sortedClassCount[0][0]
 
     # 对距离进行升序排序，取前k个
     def sortDist(self):
@@ -60,4 +99,4 @@ class knn:
 
 
 k = knn('lian_chuang\\data\\iris.data')
-print(k.normalize())
+k.normalize()
