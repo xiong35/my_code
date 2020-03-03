8eb362703e3b0e689cff29a7fee0d2bcae50b320
xiong35 <2242787668@qq.com>
2020-03-03 12:35:22 +0800

    5 feature begin

---
+++ ML_works/lian_chuang/5_feature_extract.py
@@ -0,0 +1,40 @@
+
+import numpy as np
+import pandas as pd
+
+
+class Extract:
+
+    df = None
+
+    def __init__(self, filename):
+        df = pd.read_table(filename, header=0, sep=" ", dtype=str)
+        df.drop(columns=['PassengerId', 'Name', 'Cabin',
+                         'Ticket', ], axis=1, inplace=True)
+        self.df = df
+
+    def clean(self):
+        self.df.replace(to_replace='female', value='-1',
+                        regex=True, inplace=True)
+        self.df.replace(to_replace='male', value='1', regex=True, inplace=True)
+        self.df.replace(to_replace=np.nan, value='30',
+                        regex=True, inplace=True)
+
+    def save(self):
+        self.df.to_csv("data.csv", index=False, header=True, na_rep="NULL")
+
+    def oneHot(self):
+        self.df['S'] = np.zeros(self.df.shape[0])
+        self.df['C'] = np.zeros(self.df.shape[0])
+        self.df['Q'] = np.zeros(self.df.shape[0])
+        for i in range(self.df.shape[0]):
+            embarked = self.df.loc[i, 'Embarked']
+            self.df.loc[i, embarked] = 1
+        self.df.drop(columns=['Embarked'], axis=1, inplace=True)
+        self.df.drop(self.df.columns[[-1]], axis=1, inplace=True)
+        print(self.df)
+
+
+d = Extract(R'lian_chuang\data\titanic.txt')
+d.clean()
+d.oneHot()
