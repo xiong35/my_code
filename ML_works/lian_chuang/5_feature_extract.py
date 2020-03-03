e9408949dcab1baa89902d7b1f618b83e45cdd81
xiong35 <2242787668@qq.com>
2020-03-03 14:12:26 +0800

    5 feature extract

--- ML_works/lian_chuang/5_feature_extract.py
+++ ML_works/lian_chuang/5_feature_extract.py
@@ -17,7 +17,7 @@ class Extract:
         self.df.replace(to_replace='female', value='-1',
                         regex=True, inplace=True)
         self.df.replace(to_replace='male', value='1', regex=True, inplace=True)
-        self.df.replace(to_replace=np.nan, value='30',
+        self.df.replace(to_replace=np.nan, value='30.0',
                         regex=True, inplace=True)
 
     def save(self):
@@ -32,9 +32,14 @@ class Extract:
             self.df.loc[i, embarked] = 1
         self.df.drop(columns=['Embarked'], axis=1, inplace=True)
         self.df.drop(self.df.columns[[-1]], axis=1, inplace=True)
-        print(self.df)
+
+    def normalize(self):
+        self.df = self.df[:].astype('float32')
+        self.df = (self.df-self.df.mean())/(self.df.std()) 
 
 
 d = Extract(R'lian_chuang\data\titanic.txt')
 d.clean()
 d.oneHot()
+d.normalize()
+d.save()
