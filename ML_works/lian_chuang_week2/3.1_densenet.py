5cf735da85b046dc9743919fc263bd0ed3583afa
xiong35 <2242787668@qq.com>
2020-03-11 20:56:40 +0800

    dense net for server

--- ML_works/lian_chuang_week2/3.1_densenet.py
+++ ML_works/lian_chuang_week2/3.1_densenet.py
@@ -17,20 +17,13 @@ def train(model):
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
-    x_train /= 255
     x_test /= 255
+    train_datagan = ImageDataGenerator(rescale=1./255, )
+    # test_datagen = ImageDataGenerator(rescale=1./255)
 
-    callbacks = [
-        keras.callbacks.TensorBoard(
-            log_dir='my_log',
-            histogram_freq=1,
-        )
-    ]
-
-    # 开始训练
-    hist = model.fit(x_train, y_train, batch_size=64, epochs=10,
-                     validation_data=(x_test, y_test), shuffle=True,
-                     callbacks=callbacks)
+    hist = model.fit_generator(train_datagan.flow(x_train, y_train, batch_size=32),
+                               steps_per_epoch=25,
+                               epochs=1, validation_data=(x_test, y_test), shuffle=True)
 
     history = hist.history
     acc = history['acc']
