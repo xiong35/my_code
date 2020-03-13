62b896020383bed6405f9f599dba513ce68425fa
xiong35 <2242787668@qq.com>
2020-03-13 23:56:07 +0800

    4.2.2 deuling DQN

--- ML_works/lian_chuang_week2/4.2.2_gym.py
+++ ML_works/lian_chuang_week2/4.2.2_gym.py
@@ -7,7 +7,7 @@ import numpy as np
 from collections import deque
 
 import matplotlib.pyplot as plt
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Lambda, Subtract, Add
 from keras.models import Model
 from keras.optimizers import Adam
 import keras.backend as K
@@ -22,21 +22,26 @@ class DQN:
             self.model.load_weights('dqn.h5')
             self.update_target_model()
 
-        self.memory_buffer = deque(maxlen=2000)
+        self.memory_buffer = deque(maxlen=2500)
         self.gamma = 0.95
         self.epsilon = 1.0
-        self.epsilon_decay = 0.995
+        self.epsilon_decay = 0.997
         self.epsilon_min = 0.02
 
-        self.env = gym.make('CartPole-v0')
+        self.env = gym.make('CartPole-v1')
 
     def build_model(self):
         inputs = Input(shape=(4,))
         x = Dense(16, activation='relu')(inputs)
         x = Dense(16, activation='relu')(x)
-        x = Dense(2, activation='linear')(x)
 
-        model = Model(inputs=inputs, outputs=x)
+        value = Dense(1, activation='linear')(x)
+        a = Dense(2, activation='linear')(x)
+        mean = Lambda(lambda x: K.mean(x, axis=1, keepdims=True))(a)
+        advantage = Subtract()([a, mean])
+        q = Add()([value, advantage])
+
+        model = Model(inputs=inputs, outputs=q)
 
         return model
 
@@ -45,7 +50,7 @@ class DQN:
 
     def random_choice(self, state):
         if np.random.rand() <= self.epsilon:
-             return random.randint(0, 1)
+            return random.randint(0, 1)
         else:
             q_values = self.model.predict(state)[0]
             return np.argmax(q_values)
@@ -72,7 +77,6 @@ class DQN:
             y[i][action] = target
 
         return states, y
-
 
     def train(self, episode, batch_size):
         self.model.compile(loss='mse', optimizer=Adam())
@@ -103,18 +107,18 @@ class DQN:
 
                     if count != 0 and count % 20 == 0:
                         self.update_target_model()
-            if i%5==0:
+            if i % 5 == 0:
                 train_reward.append(reward_sum)
                 train_loss.append(loss)
-            print('%%%.2f'%(100*i/episode))
-    
+            print('%%%.2f' % (100*i/episode))
+
         self.model.save_weights('dqn.h5')
         epochs = range(1, len(train_reward)+1)
-        plt.plot(epochs,train_reward,'b', label='Training reward')
+        plt.plot(epochs, train_reward, 'b', label='Training reward')
         plt.legend()
         plt.show()
         plt.clf()
-        plt.plot(epochs,train_loss,'bo', label='Training loss')
+        plt.plot(epochs, train_loss, 'bo', label='Training loss')
         plt.legend()
         plt.show()
 
@@ -137,7 +141,8 @@ class DQN:
             reward_sum += reward
 
             if done:
-                print("Reward for this episode was: {}, turns was: {}".format(reward_sum, count))
+                print("Reward for this episode was: {}, turns was: {}".format(
+                    reward_sum, count))
                 random_episodes += 1
                 reward_sum = 0
                 count = 0
@@ -147,5 +152,5 @@ class DQN:
 
 
 model = DQN()
-model.train(300, 32)
+# model.train(100, 32)
 model.play()
